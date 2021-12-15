16f511d18bd80b06e15e0c9435c0cd8bc8563b7f
Zbizu <zbirox@gmail.com>
2021-12-15 04:00:48 +0100

    removed unnecessary startup print lua version is printed at the beginning already

--- data/lib/debugging/lua_version.lua
+++ data/lib/debugging/lua_version.lua
@@ -1,5 +1,10 @@
-if type(jit) == 'table' then
-	print('>> Using ' .. jit.version)  --LuaJIT 2.0.2
-else
-	print('>> Using ' .. _VERSION)
+function printLuaVersion()
+	if type(jit) == 'table' then
+		print('>> Using ' .. jit.version)  --LuaJIT 2.0.2
+	else
+		print('>> Using ' .. _VERSION)
+	end
 end
+
+-- uncomment to print on startup
+-- printLuaVersion()
