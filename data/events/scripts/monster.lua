d1be6b7699eee2b261dd77fef38dd361b163f370
Zbizu <zbirox@gmail.com>
2021-12-20 22:02:15 +0100

    kill tracker
    Co-Authored-By: Artur Knopik <krecikondexin@gmail.com>
    Co-Authored-By: Stefan A. Brannfjell <stefan_brannfjell@live.no>

--- data/events/scripts/monster.lua
+++ data/events/scripts/monster.lua
@@ -1,4 +1,9 @@
 function Monster:onDropLoot(corpse)
+	local player = Player(corpse:getCorpseOwner())
+	if player then
+		player:updateKillTracker(self, corpse)
+	end
+	
 	if hasEventCallback(EVENT_CALLBACK_ONDROPLOOT) then
 		EventCallback(EVENT_CALLBACK_ONDROPLOOT, self, corpse)
 	end
