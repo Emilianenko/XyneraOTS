ab3f476b9a4ef9f6bb95ff840c984f2ee8eea209
Zbizu <zbirox@gmail.com>
2021-12-20 10:12:00 +0100

    clay lump script - better chance calculating

--- data/scripts/actions/others/clay_lump.lua
+++ data/scripts/actions/others/clay_lump.lua
@@ -1,17 +1,22 @@
 local config = {
-	{chance = {0.0, 1.54}, transformId = 11342, description = "This little figurine of Brog, the raging Titan, was skillfully made by |PLAYERNAME|.", achievement = true},
-	{chance = {1.54, 9.16}, transformId = 11341, description = "It was made by |PLAYERNAME| and is clearly a little figurine of.. hm, one does not recognise that yet."},
-	{chance = {9.16, 25.48}, transformId = 11340, description = "It was made by |PLAYERNAME|, whose potter skills could use some serious improvement."},
-	{chance = {25.48, 100.0}, remove = true, sound = "Aw man. That did not work out too well."}
+	{chance = 1, transformId = 11342, description = "This little figurine of Brog, the raging Titan, was skillfully made by |PLAYERNAME|.", achievement = true},
+	{chance = 9, transformId = 11341, description = "It was made by |PLAYERNAME| and is clearly a little figurine of.. hm, one does not recognise that yet."},
+	{chance = 25, transformId = 11340, description = "It was made by |PLAYERNAME|, whose potter skills could use some serious improvement."},
+	{chance = 75, remove = true, sound = "Aw man. That did not work out too well."}
 }
 
 local clayLump = Action()
 
 function clayLump.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	local random, tmpItem = math.random(0, 10000) * 0.01
+	local roll = math.random(1, 100)
+	local currentChance = 0
+	local tmpItem
+	
 	for i = 1, #config do
 		tmpItem = config[i]
-		if random >= tmpItem.chance[1] and random < tmpItem.chance[2] then
+		currentChance = currentChance + tmpItem.chance
+		
+		if roll <= currentChance then
 			item:getPosition():sendMagicEffect(CONST_ME_POFF)
 
 			if tmpItem.remove then
