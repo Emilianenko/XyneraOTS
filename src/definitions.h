6bc2b44659c9d6d7e01ef59db3d520285b91fc65
Zbizu <zbirox@gmail.com>
2022-01-08 22:02:34 +0100

    update to 12.85

--- src/definitions.h
+++ src/definitions.h
@@ -24,9 +24,9 @@ static constexpr auto STATUS_SERVER_NAME = "The Forgotten Server";
 static constexpr auto STATUS_SERVER_VERSION = "1.5 Plus";
 static constexpr auto STATUS_SERVER_DEVELOPERS = "Zbizu";
 
-static constexpr auto CLIENT_VERSION_MIN = 1280;
-static constexpr auto CLIENT_VERSION_MAX = 1281;
-static constexpr auto CLIENT_VERSION_STR = "12.81";
+static constexpr auto CLIENT_VERSION_MIN = 1285;
+static constexpr auto CLIENT_VERSION_MAX = 1285;
+static constexpr auto CLIENT_VERSION_STR = "12.85";
 
 static constexpr auto AUTHENTICATOR_DIGITS = 6U;
 static constexpr auto AUTHENTICATOR_PERIOD = 30U;
