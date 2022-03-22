2286ee138eaa61cd63643175a70916701333a50c
Zbizu <zbirox@gmail.com>
2022-03-22 16:45:16 +0100

    (upstream 3722, 3906, 3907) small changes

--- src/player.h
+++ src/player.h
@@ -114,6 +114,8 @@ using MuteCountMap = std::map<uint32_t, uint32_t>;
 static constexpr int32_t PLAYER_MAX_SPEED = 2 * std::numeric_limits<uint16_t>::max();
 static constexpr int32_t PLAYER_MIN_SPEED = 10;
 
+static constexpr int32_t NOTIFY_DEPOT_BOX_RANGE = 1;
+
 class Player final : public Creature, public Cylinder
 {
 	public:
