80bb326969b05b2b6a48a67aa5662df68e6df7d5
Zbizu <zbirox@gmail.com>
2022-03-24 17:27:14 +0100

    (upstream 3941) ec.onDropLoot code cleanup
    Co-authored-by: Evil Puncker <EPuncker@users.noreply.github.com>

--- data/scripts/eventcallbacks/monster/default_onDropLoot.lua
+++ data/scripts/eventcallbacks/monster/default_onDropLoot.lua
@@ -7,26 +7,30 @@ ec.onDropLoot = function(self, corpse)
 
 	local player = Player(corpse:getCorpseOwner())
 	local mType = self:getType()
+	local lootCreated = false
+	
 	if not player or player:getStamina() > 840 then
+		lootCreated = true
+	end
+	
+	if lootCreated then
 		local monsterLoot = mType:getLoot()
 		for i = 1, #monsterLoot do
 			local item = corpse:createLootItem(monsterLoot[i])
 			if not item then
-				print('[Warning] DropLoot:', 'Could not add loot item to corpse.')
+				print("[Warning] DropLoot: Could not add loot item to corpse.")
 			end
 		end
-
-		if player then
-			local text = ("Loot of %s: %s"):format(mType:getNameDescription(), corpse:getColorContentDescription())
-			local party = player:getParty()
-			if party then
-				party:broadcastPartyLoot(text)
-			else
-				player:sendTextMessage(MESSAGE_LOOT, text)
-			end
+	end
+	
+	if player then
+		local text
+		if lootCreated then
+			text = ("Loot of %s: %s"):format(mType:getNameDescription(), corpse:getColorContentDescription())
+		else
+			text = ("Loot of %s: nothing (due to low stamina)"):format(mType:getNameDescription())
 		end
-	else
-		local text = ("Loot of %s: nothing (due to low stamina)"):format(mType:getNameDescription())
+		
 		local party = player:getParty()
 		if party then
 			party:broadcastPartyLoot(text)
