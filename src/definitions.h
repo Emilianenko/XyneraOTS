f26c48abfdc2e858cebe0a30f3d5e869856974f3
Zbizu <zbirox@gmail.com>
2022-04-02 04:31:31 +0200

    (upstream 3986) updated to 12.86
    Co-authored-by: Evil Puncker <EPuncker@users.noreply.github.com>

--- src/definitions.h
+++ src/definitions.h
@@ -8,9 +8,9 @@ static constexpr auto STATUS_SERVER_NAME = "The Forgotten Server";
 static constexpr auto STATUS_SERVER_VERSION = "1.5 Plus";
 static constexpr auto STATUS_SERVER_DEVELOPERS = "Zbizu";
 
-static constexpr auto CLIENT_VERSION_MIN = 1285;
-static constexpr auto CLIENT_VERSION_MAX = 1285;
-static constexpr auto CLIENT_VERSION_STR = "12.85";
+static constexpr auto CLIENT_VERSION_MIN = 1286;
+static constexpr auto CLIENT_VERSION_MAX = 1286;
+static constexpr auto CLIENT_VERSION_STR = "12.86";
 
 static constexpr auto AUTHENTICATOR_DIGITS = 6U;
 static constexpr auto AUTHENTICATOR_PERIOD = 30U;
