a43d0bcfaaaedadf3151229a382c4e7058adcb81
Zbizu <zbirox@gmail.com>
2022-08-09 02:44:09 +0200

    feature: color name for admins

---
+++ data/scripts/modules/adminColorName.lua
@@ -0,0 +1,9 @@
+local creatureevent = CreatureEvent("adminColorNameLogin")
+function creatureevent.onLogin(player)
+	if player:getGroup():getAccess() then
+		player:setDisplayName(string.format('<font color="#ff0000">%s %s</font>&nbsp;&nbsp;', string.char(5), player:getName()))
+	end
+	
+	return true
+end
+creatureevent:register()
