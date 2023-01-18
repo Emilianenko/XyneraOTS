cf2918ec23fd98e517d865fcbf158e6484f26f69
Zbizu <zbirox@gmail.com>
2023-01-18 14:47:08 +0100

    remove test function

--- data/scripts/stressTest.lua
+++
@@ -1,7 +0,0 @@
-function makePlayers()
-	for accId = 101, 120 do
-		for playerId = 1, 50 do
-			db.query("INSERT INTO `players`(`name`, `account_id`) VALUES ('test " .. accId - 100 .. "-" .. playerId .. "','" .. accId .. "')")
-		end
-	end
-end
