f092f8bcfcf6c40e6fd25630a70f2c7cfa75bca7
Zbizu <zbirox@gmail.com>
2023-01-19 02:35:49 +0100

    downgrade me to 8.6

--- data/npc/scripts/tpmarker.lua
+++ data/npc/scripts/tpmarker.lua
@@ -1,67 +1,49 @@
--- wczesniejszy wyglad
--- local labelDefault = "<center><h2>|ARROWDOWN| |NPCNAME| |ARROWDOWN|</h2></center>Travel to |NPCNAME|",
-
--- wersja 2
--- local labelDefaultLeft = "<h2>|NPCNAME| |ARROWDOWN|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>"
--- local labelDefaultRight = "<h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|ARROWDOWN| |NPCNAME|</h2>"
-
-local labelDefault = '<font color="#f7df05" size="5">|NPCNAME|<br />|CENTERARROW|</font><font color="#baa802" size="5">|ARROWDOWN|</font>'
-local labelDefaultAlt = '<font color="#f7df05" size="5">|NPCNAME|<br />|CENTERARROWALT|</font><font color="#baa802" size="5">|ARROWDOWN|</font>'
-
 local markers = {
 	["Monsters"] = {
 		itemId = 1387,
 		destination = Position(1064, 992, 3),
 		effect = 178,
-		label = labelDefault,
+		color = "#f7df05",
 	},
 	["Temple"] = {
 		itemId = 1387,
 		destination = Position(1121, 959, 7),
 		effect = 178,
-		label = labelDefaultAlt,
+		color = "#f7df05",
 	},
 	["Quests"] = {
 		itemId = 1387,
 		destination = Position(1050, 1000, 5),
 		effect = 178,
-		label = labelDefault,
+		color = "#f7df05",
 	},
 	["NPC"] = {
 		itemId = 1387,
 		destination = Position(968, 1059, 7),
 		effect = 178,
-		label = labelDefault,
+		color = "#f7df05",
 	},
 	["Bosses"] = {
 		itemId = 1387,
 		-- destination = Position(968, 1059, 7),
 		effect = 178,
-		label = labelDefaultAlt,
+		color = "#f7df05",
 	},
 	["Cassino"] = {
 		itemId = 1387,
 		-- destination = Position(968, 1059, 7),
 		effect = 178,
-		label = labelDefault,
+		color = "#f7df05",
 	},
 	["Trainers"] = {
 		--itemId = 1387,
 		--destination = Position(0, 0, 0),
 		effect = 178,
-		label = labelDefault,
+		color = "#f7df05",
 	}
 }
 
-local macros = {
-	["|ARROWDOWN|"] = function(creature, pos) return "v" end,
-	["|NPCNAME|"] = function(creature, pos) return creature:getName() end,
-	["|CENTERARROW|"] = function(creature, pos) return string.rep("&nbsp;", math.floor(creature:getName():len() * 0.80)) end,
-	["|CENTERARROWALT|"] = function(creature, pos) return string.rep("&nbsp;", math.floor(creature:getName():len() * 0.80)+1) end,
-}
-
 local init = false
-local selfPos
 local effect
 
 -- tymczasowa przechowalnia na npcki
@@ -72,6 +54,37 @@ end
 
 if not TP_MARKERS_RELOAD_GUARD then
 	TP_MARKERS_RELOAD_GUARD = {}
+	TP_MARKERS_HOME = {}
+end
+
+local function animate(npcId, frameId)
+	local npc = Creature(npcId)
+	if not npc then
+		return
+	end
+	
+	local npcName = npc:getName()
+	if frameId and frameId == -1 then
+		npc:setDisplayName(" ")
+	else
+		local marker = markers[npcName]
+		if marker then
+			npc:setDisplayName(string.format('<font color="%s"><h2>%s</h2></font>%s', marker.color, npcName, string.rep("<br />", frameId or 2)))
+			
+			if marker.effect and not frameId then
+				Position(TP_MARKERS_HOME[npcId]):sendMagicEffect(marker.effect)
+			end
+		end
+	end
+	
+	if not frameId then
+		addEvent(animate, 175, npcId, 3)
+		addEvent(animate, 350, npcId, 4)
+		addEvent(animate, 525, npcId, 5)
+		addEvent(animate, 700, npcId, 6)
+		addEvent(animate, 875, npcId, -1)
+		addEvent(animate, 1500, npcId)
+	end
 end
 
 local function onInit(npcId)
@@ -81,28 +94,26 @@ local function onInit(npcId)
 	end
 	
 	local selfName = self:getName()
-	selfPos = self:getPosition()
+	local selfPos = self:getPosition()
+	
+	if not TP_MARKERS_HOME[npcId] then
+		TP_MARKERS_HOME[npcId] = {x = selfPos.x, y = selfPos.y, z = selfPos.z}
+	end
 	
 	self:setOutfit({lookTypeEx = 1548})
 	
 	local tpInfo = markers[selfName]
-	local label = tpInfo.label
-	for macro, callback in pairs(macros) do
-		label = label:gsub(macro, callback(self, selfPos))
-	end
-	
-	self:setDisplayName(label)
 	self:setDisplayMode(CREATURE_DISPLAY_MODE_PLAYER)
 	self:setHiddenHealth(true)
 	self:setPhantom(true)
-	
+
 	if TP_MARKERS_RELOAD_GUARD[npcId] then
 		-- handle reload
 		effect = tpInfo.effect
-		local tile = self:getTile()
+		local tile = Tile(TP_MARKERS_HOME[npcId])
 		if tile then
 			local prevTp = TP_MARKERS_RELOAD_GUARD[npcId]
-			if prevTp and prevTp ~= 1 then
+			if prevTp then
 				local oldTp = tile:getItemById(prevTp)
 				if oldTp then
 					oldTp:remove()
@@ -110,29 +121,35 @@ local function onInit(npcId)
 			end
 		end
 	else
+		-- move the npc one tile down
+		selfPos.y = selfPos.y + 1
+
 		-- force technical npc on top of real one to hide context menu
 		self:teleportTo(TP_MARKERS_TECHPOS)
 		Game.createNpc("TpFilter", selfPos, false, true)
 		self:teleportTo(selfPos)
+		
 		-- force a second npc to hide look
 		Game.createNpc("TpFilter", selfPos, false, true)
+		
+		animate(npcId)
 	end
 
-	-- initialize
+	--self:setDisplayName(string.format('<h1><font color="%s">%s</font></h1>%s', tpInfo.color, selfName, string.rep("<br />", 2)))
+
+	-- load tp itemid from config
 	TP_MARKERS_RELOAD_GUARD[npcId] = tpInfo.itemId or 1
-	local destination = tpInfo.destination
-	if destination then
-		local destId = tpInfo.itemId
-		if not destId then
-			markers[selfName].itemId = 1387
-			destId = 1387
-		end
 	
-		local tp = Game.createItem(destId, 1, selfPos)
-		if tp then
-			tp:setDestination(destination)
-			effect = tpInfo.effect
-		end
+	-- initialize
+	if tpInfo.itemId then
+		local destination = tpInfo.destination
+		if destination then
+			local tp = Game.createItem(tpInfo.itemId, 1, TP_MARKERS_HOME[npcId])
+			if tp then
+				tp:setDestination(destination)
+				effect = tpInfo.effect
+			end
+		end		
 	end
 end
 
@@ -146,8 +163,5 @@ end
 function onCreatureDisappear(cid) end
 function onCreatureSay(cid, type, msg) end
 function onThink()
-	if selfPos and effect and os.time() % 2 == 0 then
-		selfPos:sendMagicEffect(effect)
-	end
 	return true
 end
